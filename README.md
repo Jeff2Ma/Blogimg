e6e3a917223a0affa5e4adef7f9a68b3ff41189f
jeff2ma <mjh2316@gmail.com>
2015-04-07 16:36:36 +0800

    功能+ 安全更新
    添加删除文件、ftp 上传功能；配置文件独立为json 文件，保证安全

--- README.md
+++ README.md
@@ -1,20 +1,37 @@
-# 借助Gulp 配置的自动图片压缩小工具
+# 借助Gulp 配置的自动图片压缩小工具+自动FTP上传
 
-用于个人写博客的文章配图的图片无损压缩
+用于个人写博客的文章配图的图片无损压缩+自动上传到FTP
 
-## 其他说明
+## 配置说明
 
-git clone 后需要配置Tinypng 信息，前往官网获取到API KEY 填入如下位置：
+为了安全，将重要的配置信息放入到一个json 文件中，名为 `config.json`，该文件示例代码如下：
 
-	//默认任务
-	var gulp = require('gulp');
-	var tinypng = require('gulp-tinypng');
+	{ 
+ 	 "tinypngapi": "xxxxx",
+ 	 "sftp" : {
+    	"host" : "8.8.8.8",
+    	"user" : "username",
+    	"port" : "22",
+    	"key" : "~/.ssh/sdfsfdsf",
+    	"remotePath" :"/"
+ 	 }
+	}	
 
-	gulp.task('default', function () {
-   		 gulp.src('img/*.{png,jpg,jpeg}')
-        	.pipe(tinypng('API-KEY'))
-        	.pipe(gulp.dest('./'));
-	});
+相关内容请自行配置，`config.json`文件已经添加到`.gitignore` 文件中。
 
+## 使用说明
+
+原始图片（未压缩图片）放入到 `img` 文件夹，压缩操作后自动到项目文件目录下。
+
+	gulp clean
 	
-一个邮箱可以免费申请每个月500 次API 调用，建议用自己的。
+在进行压缩图片操作前存档+删除之前的旧文件内容
+
+	gulp
+
+默认任务，图片压缩。因为外国服务器原因，压缩有时候非常慢，考虑挂代理
+
+	gulp upload
+	
+上传压缩后的文件到远程FTP 服务器上
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,55 @@
 //默认任务
-var gulp = require('gulp');
-var tinypng = require('gulp-tinypng');
+var gulp = require('gulp'),
+tinypng = require('gulp-tinypng'),
+sftp = require('gulp-sftp'),
+copy = require('gulp-copy'),
+clean = require('gulp-clean'),
+config = require('./config.json');
 
+//默认任务
 gulp.task('default', function () {
     gulp.src('img/*.{png,jpg,jpeg}')
-        .pipe(tinypng('TXLweO1Lvlxnq9-Yg4sDg0WKnPlHls3d'))
+        .pipe(tinypng(config.tinypngapi))
         .pipe(gulp.dest('./'));
+});
+
+//copy
+gulp.task('copy', function() {
+   gulp.src('img/*.{png,jpg,jpeg}')
+   .pipe(gulp.dest('./old'));
+});
+
+//删除
+gulp.task('delete', function () {
+    return gulp.src(['img/*.{png,jpg,jpeg}','./*.{png,jpg,jpeg}'], {read: false})
+        .pipe(clean());
+});
+
+//清理任务
+gulp.task('clean', function(){
+  gulp.run('copy');
+  gulp.run('delete');
+});
+
+//上传任务
+gulp.task('upload', function () {
+	function checkTime(i) {
+          if (i < 10) {
+              i = "0" + i
+          }
+          return i
+      }
+
+	var d = new Date();
+	var year = d.getFullYear();
+	var month = checkTime(d.getMonth() + 1);
+
+    return gulp.src('./*.{png,jpg,jpeg}')
+        .pipe(sftp({
+            host: config.sftp.host,
+            user: config.sftp.user,
+            port: config.sftp.port,
+            key: config.sftp.key,
+            remotePath: config.sftp.remotePath+year+'/'+month
+        }));
 });
